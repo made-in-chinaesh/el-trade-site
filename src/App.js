dc8c21a3aad86e5a89c84cb539355bffbac9f2d7
MalikovQimica <nurbekmalikov010@gmail.com>
2026-04-05 12:18:22 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div>
+			<h2>Hello, World!</h2>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt odio
+				corporis neque similique tempore officia repudiandae officiis beatae
+				voluptatem at accusamus aperiam veritatis excepturi sapiente, magnam ut
+				magni fugiat numquam suscipit non iste. Nemo sunt dolor, suscipit porro,
+				magni aspernatur nesciunt, recusandae deserunt accusantium rerum officia
+				odit deleniti. Quo ex eveniet libero dignissimos nihil quia vitae, quod
+				accusamus neque modi, reiciendis eum aperiam explicabo! Obcaecati
+				cupiditate nostrum eligendi voluptatibus tempore dicta blanditiis ab
+				libero necessitatibus placeat recusandae, laboriosam, consequatur aut
+				consequuntur laborum vero laudantium dolorum sint vitae quam accusamus.
+				Fuga dolore nobis autem nemo fugiat officia veritatis aliquam obcaecati
+				iure!
+			</p>
+		</div>
+	)
 }
-
-export default App;
